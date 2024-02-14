7cce8f69f086af5c626a7eb62a6d617b6e5a206b
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-13 20:20:30 -0700

    Delete src/main/java/org/itson/bdavanzadas/banco directory

--- src/main/java/org/itson/bdavanzadas/banco/Banco.java
+++
@@ -1,9 +0,0 @@
-package org.itson.bdavanzadas.banco;
-
-public class Banco {
-
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
-        System.out.println("Hola Abelardo");
-    }
-}
