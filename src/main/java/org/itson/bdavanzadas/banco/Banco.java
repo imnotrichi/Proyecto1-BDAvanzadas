3469b848b883204ce4c71b885c4d23242743de04
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-13 19:04:27 -0700

    Primer Cambio

--- src/main/java/org/itson/bdavanzadas/banco/Banco.java
+++ src/main/java/org/itson/bdavanzadas/banco/Banco.java
@@ -3,6 +3,6 @@ package org.itson.bdavanzadas.banco;
 public class Banco {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo!");
     }
 }
